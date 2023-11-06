c329f2a01020b55f3d574041640dc756af35f510
hishamgit <mohammedhishamk81@gmail.com>
2023-11-06 21:46:41 +0530

    Routes CRUD operations on books database completed

--- backend/index.js
+++ backend/index.js
@@ -47,6 +47,51 @@ app.get('/book',async(req,res)=>{
         res.status(500).send({message:error.message})
     }
 })
+//route to get one book by Id
+app.get('/book/:id',async(req,res)=>{
+    try {
+        const id=req.params.id;                //same as   const {id}=req.params
+        const book=await Book.findById(id)
+        res.status(200).json({book});
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send({message:error.message})
+    }
+})
+//route to update a book data
+app.put('/book/:id',async(req,res)=>{
+    try {
+        if(!req.body.title || !req.body.author || !req.body.publishYear){
+            res.status(400).send({message: "send all required fields :title,author,publishYear"});
+        }
+        const {id}=req.params;
+        const result=await Book.findByIdAndUpdate(id,req.body)
+        if(!result){
+            res.status(400).send({message: "book not found"});
+        }else{
+            res.status(200).send({message:"Updated successfully"});
+        }
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send({message:error.message})
+    }
+})
+//route to delete a book
+//route to get one book by Id
+app.delete('/book/:id',async(req,res)=>{
+    try {
+        const {id}=req.params;               //same as   const id=req.params.id; 
+        const result=await Book.findByIdAndDelete(id)
+        if(!result){
+            res.status(400).send({message: "book not found"});
+        }else{
+            res.status(200).send({message:"Deleted successfully"});
+        }
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send({message:error.message})
+    }
+})
 
 mongoose
   .connect(mongoDbURL)
